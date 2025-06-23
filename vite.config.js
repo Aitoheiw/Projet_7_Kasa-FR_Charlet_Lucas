5b145b3ac8847625faef588b0ec3f03352d2bace
Aitoheiw <124202509+Aitoheiw@users.noreply.github.com>
2025-06-23 23:13:44 +0200

    Rebuild avec bonne base Vite pour GitHub Pages

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/Projet_7_Kasa-FR_Charlet_Lucas/",
-  plugins: [react()],
-});
