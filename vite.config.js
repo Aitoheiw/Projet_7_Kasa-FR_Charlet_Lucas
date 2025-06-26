d0a0a2d6fc1b5b75d0b1b4e4e78f2b3b8bbc3386
Aitoheiw <124202509+Aitoheiw@users.noreply.github.com>
2025-06-26 21:21:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Projet_7_Kasa-FR_Charlet_Lucas/",
+  // base: "/Projet_7_Kasa-FR_Charlet_Lucas/",
   plugins: [react()],
 });
